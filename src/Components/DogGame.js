e50168e82f2fc9b93ad2a1a5c1b4109748d79428
Mathew <doakmath@gmail.com>
2025-01-07 17:07:53 -0600

    added css styling and positioning containers

--- src/Components/DogGame.js
+++ src/Components/DogGame.js
@@ -62,9 +62,9 @@ const DogGame = () => {
     return(
         <>
         <div className="game-container">
+            <h1 className="game-title">Wesley's Dog Game</h1>
             {counter === 0 &&
             <div>
-                <h1 className="game-title">Wesley's Dog Game</h1>
                 <button
                     onClick={() => {
                         increaseCounter();
@@ -73,116 +73,115 @@ const DogGame = () => {
                 </button>
             </div>}
             {counter === 1 &&
-            <div className = "button-container">
+            <>
+            <div>
                 <h1>Choose your favorite!</h1>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setDog1(choice1);
-                        }}><img src={choice1} alt='dog1' />
-                    </button>
-                </div>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setDog1(choice2);
-                        }}><img src={choice2} alt='dog2' />
-                    </button>
-                </div>
             </div>
+            <div className="image-grid">
+                <button
+                    onClick={() => {
+                        increaseCounter();
+                        setDog1(choice1);
+                    } }><img src={choice1} alt='dog1' />
+                </button>
+                <button
+                    onClick={() => {
+                        increaseCounter();
+                        setDog1(choice2);
+                    } }><img src={choice2} alt='dog2' />
+                </button>
+            </div>
+            </>
             }
             {counter === 2 &&
-            <div className = "button-container">
+            <>
+            <div>
                 <h1>Choose your favorite!</h1>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setDog2(choice3);
-                        }}><img src={choice3} alt='dog3' />
-                    </button>
-                </div>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setDog2(choice4);
-                        }}><img src={choice4} alt='dog4' />
-                    </button>
-                </div>
             </div>
+            <div className = "image-grid">
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setDog2(choice3);
+                    }}><img src={choice3} alt='dog3' />
+                </button>
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setDog2(choice4);
+                    }}><img src={choice4} alt='dog4' />
+                </button>
+            </div>
+            </>
             }
             {counter === 3 &&
-            <div className = "button-container">
+            <>
+            <div>
                 <h1>Choose your favorite!</h1>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setDog3(choice5);
-                        }}><img src={choice5} alt='dog5' />
-                    </button>
-                </div>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setDog3(choice6);
-                        }}><img src={choice6} alt='dog6' />
-                    </button>
-                </div>
             </div>
+            <div className = "image-grid">
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setDog3(choice5);
+                    }}><img src={choice5} alt='dog5' />
+                </button>
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setDog3(choice6);
+                    }}><img src={choice6} alt='dog6' />
+                </button>
+            </div>
+            </>
             }
             {counter === 4 &&
-            <div className = "button-container">
+            <>
+            <div>
                 <h1>Choose your favorite!</h1>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setWinner(dog1);
-                        }}><img src={dog1} alt='winner dog 1' />
-                    </button>
-                </div>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setWinner(dog2);
-                        }}><img src={dog2} alt='winner dog 2' />
-                    </button>
-                </div>
             </div>
+            <div className = "image-grid">
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setWinner(dog1);
+                    }}><img src={dog1} alt='winner dog 1' />
+                </button>
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setWinner(dog2);
+                    }}><img src={dog2} alt='winner dog 2' />
+                </button>
+            </div>
+            </>
             }
             {counter === 5 &&
-            <div className = "button-container">
+            <>
+            <div>
                 <h1>Choose your favorite!</h1>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                        }}><img src={winner} alt='winner dog' />
-                    </button>
-                </div>
-                <div className = "button-box">
-                    <button
-                        onClick = {() => {
-                            increaseCounter();
-                            setWinner(dog3);
-                        }}><img src={dog3} alt='winner dog 3' />
-                    </button>
-                </div>
             </div>
+            <div className = "image-grid">
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                    }}><img src={winner} alt='winner dog' />
+                </button>
+                <button
+                    onClick = {() => {
+                        increaseCounter();
+                        setWinner(dog3);
+                    }}><img src={dog3} alt='winner dog 3' />
+                </button>
+            </div>
+            </>
             }
             {counter === 6 &&
             <div className="winner-container">
                 <div className="winner-image">
-                    <h2>Winner</h2>
-                    <h3>Breed:</h3>
-                    <h3>{extractBreedFromImageUrl(winner)}</h3>
+                    <h2>Winner!</h2>
                     <img src={winner} alt="winner dog" />
+                    <h3>Breed: {extractBreedFromImageUrl(winner)}</h3>
                     <div className="play-again-button">
                         <button
                             onClick= {() => {
